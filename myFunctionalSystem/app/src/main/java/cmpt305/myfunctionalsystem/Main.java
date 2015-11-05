210a8b1f4c619143cc92e3b5495efbb25dc8478f
cmoorhouse <cody.moorhouse@icloud.com>
2015-11-05 16:33:08 -0700

    Add myClassSearch

--- myFunctionalSystem/app/src/main/java/cmpt305/myfunctionalsystem/Main.java
+++ myFunctionalSystem/app/src/main/java/cmpt305/myfunctionalsystem/Main.java
@@ -1,5 +1,6 @@
 package cmpt305.myfunctionalsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,6 @@ public class Main extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -42,11 +35,27 @@ public class Main extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        switch (id) {
+            case R.id.action_my_class_search:
+                launchActivity(myClassSearch.class);
+                break;
+
+            case R.id.action_dylan:
+                launchActivity(myClassSearch.class);
+                break;
+        }
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchActivity(Class<?> className) {
+        Intent intent = new Intent(this, className);
+        startActivity(intent);
+
     }
 }
