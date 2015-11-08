115f70fe370cd6c8d72d215905c6589417b40b28
cmoorhouse <cody.moorhouse@icloud.com>
2015-11-07 19:19:18 -0700

    Separated mySearch screens

--- myFunctionalSystem/app/src/main/java/cmpt305/myfunctionalsystem/searchCourses.java
+++ myFunctionalSystem/app/src/main/java/cmpt305/myfunctionalsystem/searchCourses.java
@@ -2,6 +2,8 @@ package cmpt305.myfunctionalsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,50 +13,28 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class myClassSearch extends AppCompatActivity {
+public class searchCourses extends AppCompatActivity {
 
-    private String[] termNames = {"Fall 2015",
-                                  "Winter 2016",
-                                  "Spring/Summer 2016",
-                                  "Fall 2016",
-                                  "Winter 2017"};
+    private String[] courseNames = {"ACCT 111\nFinancial Accounting I",
+                                    "ACCT 161\nFinancial Accounting II",
+                                    "ACCT 164\nPractical Tax Applications",
+                                    "ACCT 211\nFinancial Accounting III",
+                                    "ACCT 214\nIntegrated Accounting Systems",
+                                    "ACCT 215\nQuantitative Decision Support",
+                                    "ACCT 218\nManagement Accounting I",
+                                    "ACCT 261\nFinancial Accounting IV",
+                                    "ACCT 268\nManagement Accounting II",
+                                    "ACCT 275\nAccounting Information Systems"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_class_search);
-
-        populateTerms();
-        registerClickCallBack();
-
+        setContentView(R.layout.activity_search_courses);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_my_class_search:
-                launchActivity();
-                break;
-        }
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) { return true; }
-
-        return super.onOptionsItemSelected(item);
+        populateCourses();
+        registerClickCallBack();
     }
 
     @Override
@@ -82,28 +62,48 @@ public class myClassSearch extends AppCompatActivity {
         super.onDestroy();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_my_class_search:
+                launchActivity();
+                break;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) { return true; }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private void registerClickCallBack() {
-        ListView list = (ListView) findViewById(R.id.myClassSearchView);
+        ListView list = (ListView) findViewById(R.id.listCourses);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-
-                if (termNames[position].compareTo("Fall 2015") == 0) { launchDepartments(); }
                 // do stuff
             }
         });
     }
 
-    private void populateTerms() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, termNames);
+    private void populateCourses() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, courseNames);
 
-        ListView list = (ListView) findViewById(R.id.myClassSearchView);
+        ListView list = (ListView) findViewById(R.id.listCourses);
         list.setAdapter(adapter);
-    }
-
-    public void launchDepartments() {
-        Intent intent = new Intent(this, searchDepartments.class);
-        startActivity(intent);
     }
 
     public void launchActivity() {
---
+++ myFunctionalSystem/app/src/main/java/cmpt305/myfunctionalsystem/searchDepartments.java
@@ -0,0 +1,125 @@
+package cmpt305.myfunctionalsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class searchDepartments extends AppCompatActivity {
+
+    private String[] departmentNames = {"Accounting",
+                                        "Acupuncture",
+                                        "Advanced Cardiac Life Support",
+                                        "Anthropology",
+                                        "Art",
+                                        "Arts and Cultural Management",
+                                        "Astronomy",
+                                        "Audiovisual Communication",
+                                        "Basic Life Support",
+                                        "Biochemistry",
+                                        "Biology",
+                                        "Botany",
+                                        "Business",
+                                        "Chemistry"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_departments);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        populateDepartments();
+        registerClickCallBack();
+    }
+
+    private void populateDepartments() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, departmentNames);
+
+        ListView list = (ListView) findViewById(R.id.listDepartments);
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_my_class_search:
+                launchActivity();
+                break;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) { return true; }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void registerClickCallBack() {
+        ListView list = (ListView) findViewById(R.id.listDepartments);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+
+                if (departmentNames[position].compareTo("Accounting") == 0) { launchCourses(); }
+
+                // do stuff
+            }
+        });
+    }
+
+    public void launchCourses() {
+        Intent intent = new Intent(this, searchCourses.class);
+        startActivity(intent);
+    }
+
+    public void launchActivity() {
+        Intent intent = new Intent(this, myClassSearch.class);
+        startActivity(intent);
+    }
+}
